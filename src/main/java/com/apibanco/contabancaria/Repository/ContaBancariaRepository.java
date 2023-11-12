1369029c2e02f829889d7d2186996f5e8c1658f1
silvamaciel <macieljunio.si@gmail.com>
2023-11-12 00:06:01 -0300

    Repositorios

---
+++ src/main/java/com/apibanco/contabancaria/Repository/ContaBancariaRepository.java
@@ -0,0 +1,14 @@
+package com.apibanco.contabancaria.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apibanco.contabancaria.Entitys.ContaBancaria;
+
+@Repository
+public interface ContaBancariaRepository  extends JpaRepository<ContaBancaria, Long>{
+    ContaBancaria findByNumeroDaConta(String numeroDaConta);
+    ContaBancaria findContaBancariaById(Long id);
+    ContaBancaria saveContaBancaria(ContaBancaria account);
+    void removeContaBancaria(ContaBancaria account);
+}
---
+++ src/main/java/com/apibanco/contabancaria/Repository/TransacaoRepository.java
@@ -0,0 +1,14 @@
+package com.apibanco.contabancaria.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apibanco.contabancaria.Entitys.Transacao;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+    List<Transacao> findByContaId(Long contaId);
+    Transacao saveTransacao(Transacao transaction);
+}
