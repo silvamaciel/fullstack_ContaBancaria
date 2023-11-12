1858b5b1cb1ce44cafdab1ff9de6ca88923c538d
silvamaciel <macieljunio.si@gmail.com>
2023-11-12 00:07:00 -0300

    Ajustes nas entidades

--- src/main/java/com/apibanco/contabancaria/Entitys/ContaBancaria.java
+++ src/main/java/com/apibanco/contabancaria/Entitys/ContaBancaria.java
@@ -5,12 +5,17 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 
 @Entity
 public class ContaBancaria {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String numeroDaConta;
     private double saldo;
@@ -20,6 +25,45 @@ public class ContaBancaria {
     private List<Transacao> transacoes;
 
     public ContaBancaria() {
+    }
+
+    public ContaBancaria(Long id, String numeroDaConta, double saldo, List<Transacao> transacoes) {
+        this.id = id;
+        this.numeroDaConta = numeroDaConta;
+        this.saldo = saldo;
+        this.transacoes = transacoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(String numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
     }
 
     
--- src/main/java/com/apibanco/contabancaria/Entitys/Transacao.java
+++ src/main/java/com/apibanco/contabancaria/Entitys/Transacao.java
@@ -21,4 +21,57 @@ public class Transacao {
     @JoinColumn(name = "conta_id")
     private ContaBancaria conta;
 
+    public Transacao(Long id, String tipo, double valor, Date dataHora, ContaBancaria conta) {
+        this.id = id;
+        this.tipo = tipo;
+        this.valor = valor;
+        this.dataHora = dataHora;
+        this.conta = conta;
+    }
+
+    
+    public Transacao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public ContaBancaria getConta() {
+        return conta;
+    }
+
+    public void setConta(ContaBancaria conta) {
+        this.conta = conta;
+    }
+
+    
 }
